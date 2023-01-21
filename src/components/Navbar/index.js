2ffb9a88d19a1ca4d8c0e95c2cbb67ebd12219e1
thatguyonfire240 <marqsandstrom@gmail.com>
2023-01-20 16:31:11 -0800

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from "react"
+
+import {
+    Nav,
+    NavLogo,
+    NavLink,
+    Bars,
+    NavMenu,
+    NavBtn,
+    NavBtnLink,
+} from "./NavbarElements"
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavLogo to="/">
+                    Logo
+                </NavLogo>
+                <Bars />
+
+                <NavMenu>
+                    <NavLink to="/" activeStyle={{ color:'black' }}>
+                        Home
+                    </NavLink>
+                    <NavLink to="/about" activeStyle={{ color:'black' }}>
+                        About
+                    </NavLink>
+                    <NavLink to="/contact" activeStyle={{ color:'black' }}>
+                        Contact
+                    </NavLink>
+                    <NavBtn>
+                        <NavBtnLink to="/">Test Button</NavBtnLink>
+                    </NavBtn>
+                </NavMenu>
+            </Nav>
+        </>
+    )
+}
+export default Navbar
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+const Home = () => {
+    return (    
+      <div id="div1">
+        <div id="center">
+        <h1>Hello Myanmar!</h1>
+        </div>
+      </div>
+
+    );
+  };
+  
+  export default Home;
+  
